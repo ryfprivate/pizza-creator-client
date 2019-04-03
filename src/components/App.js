9061ae607dca18e7abd419a09e249e07988a544e
rayfeng <ryfprivate@outlook.com>
2019-04-03 20:48:38 +1100

    Added Section Component

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,20 @@
 import React from "react";
 import styled from "styled-components";
 
+import Section from "./Section";
+
 const Title = styled.h1`
   color: red;
 `;
 class App extends React.Component {
   render() {
-    return <Title>Hello World</Title>;
+    return (
+      <>
+        <Section title="Welcome">
+          <p>Hello world!</p>
+        </Section>
+      </>
+    );
   }
 }
 
---
+++ src/components/Section.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default ({ title, children }) => {
+  return (
+    <div>
+      <h2>{title}</h2>
+      {children}
+    </div>
+  );
+};
