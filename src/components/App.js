eaab477d80d9c0863d8f14ab59b1a64c88b92089
rayfeng <ryfprivate@outlook.com>
2019-04-06 14:13:54 +1100

    Added basic Size component

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 
 import Section from "./Section";
 import DetailsForm from "./DetailsForm";
+import Sizes from "./Sizes";
 
 const Page = styled.div`
   font: 300 16px/1.4 -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica,
@@ -14,6 +15,9 @@ class App extends React.Component {
       <Page>
         <Section title="Enter your details">
           <DetailsForm />
+        </Section>
+        <Section title="Select your size">
+          <Sizes />
         </Section>
       </Page>
     );
---
+++ src/components/Sizes.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default () => {
+  return (
+    <div>
+      <p>Large</p>
+      <p>Medium</p>
+      <p>Small</p>
+    </div>
+  );
+};
