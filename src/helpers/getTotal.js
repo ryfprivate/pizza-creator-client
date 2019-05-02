9fe63d03807c167c46e71b6d15affc5f3a81d2ad
rayfeng <ryfprivate@outlook.com>
2019-05-02 21:04:26 +1000

    Moved getTotal to helpers folder

--- src/helpers/getTotal.js
+++ src/helpers/getTotal.js
@@ -3,5 +3,5 @@ export default ({ selectedSize, selectedToppings }) => {
   selectedToppings.forEach(({ price, amount }) => {
     total = total + price * amount;
   });
-  return total;
+  return total.toFixed(2);
 };
