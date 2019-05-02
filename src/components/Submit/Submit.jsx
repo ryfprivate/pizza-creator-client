feb022529daf9f55fa4ac4f5f0b25fdb92ce7c74
rayfeng <ryfprivate@outlook.com>
2019-05-02 18:14:37 +1000

    Turned Submit section into component

---
+++ src/components/Submit/Submit.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  padding: 1rem 2rem;
+`;
+
+const Button = styled.button`
+  width: 100%;
+  background: #98c550;
+  border: none;
+  padding: 10px 15px;
+  text-align: center;
+  font-size: 1.5rem;
+  color: white;
+  border-radius: 5px;
+  cursor: pointer;
+`;
+
+export default () => {
+  return (
+    <Container>
+      <Button type="submit">Place Your Order</Button>
+    </Container>
+  );
+};
