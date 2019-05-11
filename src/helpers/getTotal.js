3b3b24718e6ba0ea8b6d66dbf4ab96964e4186a3
rayfeng <ryfprivate@outlook.com>
2019-05-11 16:04:28 +1000

    Edited getTotal to account for null selectedSize

--- src/helpers/getTotal.js
+++ src/helpers/getTotal.js
@@ -1,5 +1,5 @@
 export default ({ selectedSize, selectedToppings }) => {
-  let total = selectedSize.price;
+  let total = selectedSize ? selectedSize.price : 0;
   selectedToppings.forEach(({ price, amount }) => {
     total = total + price * amount;
   });
